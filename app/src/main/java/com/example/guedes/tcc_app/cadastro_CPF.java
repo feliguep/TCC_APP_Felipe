7c4438348bdd20fac610bd6ef6678b377412453c
feliguep <feliguep@gmail.com>
2017-05-22 17:10:15 -0300

    tcc app
    tcc

---
+++ app/src/main/java/com/example/guedes/tcc_app/cadastro_CPF.java
@@ -0,0 +1,13 @@
+package com.example.guedes.tcc_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastro_CPF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_CPF);
+    }
+}
